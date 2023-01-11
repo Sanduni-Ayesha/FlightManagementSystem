b702c03fa501c30071f3160b31e2a1e5574fc86b
HarshanaEranda <harshawije1998@gmail.com>
2023-01-11 11:16:56 +0000

    added changes by harshana

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FlightScreenComponent } from './componet/Flight/flight-screen/flight-screen.component';
-import {CreateRouteTableComponent} from "./componet/Router/create-route-table/create-route-table.component";
-import {AddRouteFormComponent} from "./componet/Router/add-route-form/add-route-form.component";
+import { FlightScreenComponent } from './component/Flight/flight-screen/flight-screen.component';
+import {CreateRouteTableComponent} from "./component/Router/create-route-table/create-route-table.component";
+import {AddRouteFormComponent} from "./component/Router/add-route-form/add-route-form.component";
 
 const routes: Routes = [
   {path: 'flight', component: FlightScreenComponent},
