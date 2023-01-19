2f798d4908836ca428c0b8d72c62a12092c104e1
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-01-19 17:56:28 +0000

    routes screen styling completed

--- src/app/component/Route/route-screen/route-screen.component.ts
+++ src/app/component/Route/route-screen/route-screen.component.ts
@@ -1,60 +1,81 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, ɵElement, ɵValue} from "@angular/forms";
-import {MatDialog} from "@angular/material/dialog";
-import {AddRouteFormComponent} from "../add-route-form/add-route-form.component";
-import {Observable} from "rxjs";
-import {map, startWith} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { FormControl, ɵElement, ɵValue } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { AddRouteFormComponent } from '../add-route-form/add-route-form.component';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
 
 const userData = [
-  {id: 1, departureAirport: "Colombo", arrivalAirport: "Dubai", mileage: "36", duration: "20"},
-  {id: 2, departureAirport: "London", arrivalAirport: "Colombo", mileage: "280", duration: "20"},
-  {id: 3, departureAirport: "Katunayaka", arrivalAirport: "Tokyo", mileage: "287", duration: "220"},
-  {id: 4, departureAirport: "Singapore", arrivalAirport: "Doha", mileage: "283", duration: "260"}
+  {
+    id: 1,
+    departureAirport: 'Colombo',
+    arrivalAirport: 'Dubai',
+    mileage: '36',
+    duration: '20',
+  },
+  {
+    id: 2,
+    departureAirport: 'London',
+    arrivalAirport: 'Colombo',
+    mileage: '280',
+    duration: '20',
+  },
+  {
+    id: 3,
+    departureAirport: 'Katunayaka',
+    arrivalAirport: 'Tokyo',
+    mileage: '287',
+    duration: '220',
+  },
+  {
+    id: 4,
+    departureAirport: 'Singapore',
+    arrivalAirport: 'Doha',
+    mileage: '283',
+    duration: '260',
+  },
 ];
 const columnSchema = [
   {
-    key: "departureAirport",
-    type: "text",
-    label: "Departure Airport"
+    key: 'departureAirport',
+    type: 'text',
+    label: 'Departure Airport',
   },
   {
-    key: "arrivalAirport",
-    type: "text",
-    label: "Arrival Airport"
+    key: 'arrivalAirport',
+    type: 'text',
+    label: 'Arrival Airport',
   },
   {
-    key: "mileage",
-    type: "text",
-    label: "Mileage/Km"
-  }
-  ,
-  {
-    key: "duration",
-    type: "text",
-    label: "Duration/Hours"
-  }
-  ,
-  {
-    key: "isEdit",
-    type: "isEdit",
-    label: ""
+    key: 'mileage',
+    type: 'text',
+    label: 'Mileage/Km',
   },
   {
-    key: "isDelete",
-    type: "isDelete",
-    label: ""
-  }
-]
-
+    key: 'duration',
+    type: 'text',
+    label: 'Duration/Hours',
+  },
+  {
+    key: 'isEdit',
+    type: 'isEdit',
+    label: '',
+  },
+  {
+    key: 'isDelete',
+    type: 'isDelete',
+    label: '',
+  },
+];
 
 @Component({
   selector: 'app-route-screen',
   templateUrl: './route-screen.component.html',
-  styleUrls: ['./route-screen.component.scss']
+  styleUrls: ['../../../styles/main-screen.scss'],
 })
 export class RouteScreenComponent implements OnInit {
   columnsSchema: any = columnSchema;
-  dataSet: any = userData
+  dataSet: any = userData;
   temporaryDataSet: any = userData;
 
   arrivalControl = new FormControl('');
@@ -65,8 +86,7 @@ export class RouteScreenComponent implements OnInit {
   private departureAirportArray: string[] | undefined;
   errorMessage: string | undefined;
 
-  constructor(public dialog: MatDialog) {
-  }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit() {
     this.arrivalAirportArray = Array.from(
@@ -76,17 +96,23 @@ export class RouteScreenComponent implements OnInit {
       new Set(userData.map((col) => col.departureAirport))
     );
 
-    this.filteredArrivalAirport = this.arrivalControl.valueChanges.pipe(startWith(''),
-      map(value => this.filterAirport(value || '', this.arrivalAirportArray)),
+    this.filteredArrivalAirport = this.arrivalControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this.filterAirport(value || '', this.arrivalAirportArray))
     );
-    this.filteredDepartureAirport = this.departureControl.valueChanges.pipe(startWith(''),
-      map(value => this.filterAirport(value || '', this.departureAirportArray)),
+    this.filteredDepartureAirport = this.departureControl.valueChanges.pipe(
+      startWith(''),
+      map((value) =>
+        this.filterAirport(value || '', this.departureAirportArray)
+      )
     );
   }
 
   private filterAirport(value: string, ArrayData: any): string[] {
     const filterValue = value.toLowerCase();
-    return ArrayData.filter((option: any) => option.toLowerCase().includes(filterValue));
+    return ArrayData.filter((option: any) =>
+      option.toLowerCase().includes(filterValue)
+    );
   }
 
   openForm(): void {
@@ -99,7 +125,6 @@ export class RouteScreenComponent implements OnInit {
         id: '',
         rowData: '',
       },
-
     });
   }
 
@@ -107,19 +132,27 @@ export class RouteScreenComponent implements OnInit {
     //find the index that need to be deleted
     const index = this.dataSet.findIndex((value: any) => value.id === id);
     this.dataSet.splice(index, 1);
-
   }
 
   search() {
-    if (this.dataSet.find((obj: any) => (obj.departureAirport === this.departureControl.value) ||
-      (obj.arrivalAirport === this.arrivalControl.value))) {
+    if (
+      this.dataSet.find(
+        (obj: any) =>
+          obj.departureAirport === this.departureControl.value ||
+          obj.arrivalAirport === this.arrivalControl.value
+      )
+    ) {
       this.temporaryDataSet = this.dataSet;
       const filterAirportData = this.dataSet.filter((obj: any) => {
-        return obj.departureAirport === this.departureControl.value || obj.arrivalAirport === this.arrivalControl.value;
+        return (
+          obj.departureAirport === this.departureControl.value ||
+          obj.arrivalAirport === this.arrivalControl.value
+        );
       });
-      this.dataSet = filterAirportData
+      this.dataSet = filterAirportData;
     } else {
-      this.errorMessage = "The Searching route is not available in the system!!!"
+      this.errorMessage =
+        'The Searching route is not available in the system!!!';
     }
   }
 
@@ -137,9 +170,6 @@ export class RouteScreenComponent implements OnInit {
         id: id,
         rowData: this.dataSet[id - 1],
       },
-
     });
-
   }
-
 }
