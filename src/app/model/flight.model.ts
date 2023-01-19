f37de26607a4003928cf475d725849c904732f1d
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-01-19 18:19:24 +0000

    create flight model interface

---
+++ src/app/model/flight.model.ts
@@ -0,0 +1,7 @@
+export interface Flight {
+  departureAirport: string;
+  arrivalAirport: string;
+  flightNo: number;
+  departureTime: Date;
+  arrivalTime: Date;
+}
--- src/app/model/flightModel.ts
+++
@@ -1,8 +0,0 @@
-/*
-export class flight{
-  departureAirport: string;
-  arrivalAirport: string;
-  flightNo: number;
-  departuretime: Date = new Date();
-  arrivaltime: Date = new Date();
-}*/
