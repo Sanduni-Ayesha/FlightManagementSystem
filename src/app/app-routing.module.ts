c461ac8829a76a4a9f30150639d3759524a0b413
HarshanaEranda <harshawije1998@gmail.com>
2023-01-19 03:36:34 +0000

    Changed button name

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FlightScreenComponent } from './component/Flight/flight-screen/flight-screen.component';
 import {CreateRouteTableComponent} from "./component/Router/create-route-table/create-route-table.component";
-import {AddRouteFormComponent} from "./component/Router/add-route-form/add-route-form.component";
 
 const routes: Routes = [
   {path: 'flight', component: FlightScreenComponent},
