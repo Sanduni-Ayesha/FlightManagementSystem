cad98d226d88cd1360e3202a44a0c51a4b8699af
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-01-23 05:18:24 +0000

    create Flight model class

--- src/app/model/flight.model.ts
+++ src/app/model/flight.model.ts
@@ -1,8 +1,24 @@
-export interface Flight {
-  id: number;
-  departureAirport: string;
-  arrivalAirport: string;
-  flightNo: number;
-  departureTime: Date;
-  arrivalTime: Date;
+export class Flight {
+  private id: number;
+  private departureAirport: string;
+  private arrivalAirport: string;
+  private flightNo: string;
+  private departureTime: Date;
+  private arrivalTime: Date;
+
+  constructor(
+    id: number,
+    departureAirport: string,
+    arrivalAirport: string,
+    flightNo: string,
+    departureTime: Date,
+    arrivalTime: Date
+  ) {
+    this.id = id;
+    this.departureAirport = departureAirport;
+    this.arrivalAirport = arrivalAirport;
+    this.flightNo = flightNo;
+    this.departureTime = departureTime;
+    this.arrivalTime = arrivalTime;
+  }
 }
