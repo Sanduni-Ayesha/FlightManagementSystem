b3b28799b6c8e980e045d19144c774499c502b40
HarshanaEranda <harshawije1998@gmail.com>
2023-01-29 22:38:02 +0000

    Added getRoutes method

--- Back-end/src/main/java/com/example/Backend/services/RouteService.java
+++ Back-end/src/main/java/com/example/Backend/services/RouteService.java
@@ -1,4 +1,18 @@
 package com.example.Backend.services;
 
+import com.example.Backend.models.Route;
+import com.example.Backend.repository.RouteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class RouteService {
+    @Autowired
+    private RouteRepository routeRepository;
+
+    public List<Route> getRoutes(){
+       return routeRepository.getAllRoutes();
+    }
 }
