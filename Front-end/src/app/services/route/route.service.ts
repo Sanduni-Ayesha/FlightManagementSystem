ce8aa347d1d7a19403b3b3a6a063e5aa72fad5f0
HarshanaEranda <harshawije1998@gmail.com>
2023-01-30 04:15:42 +0000

    Added getAllRoutes method

--- Front-end/src/app/services/route/route.service.ts
+++ Front-end/src/app/services/route/route.service.ts
@@ -1,12 +1,16 @@
 import {Injectable, OnInit} from '@angular/core';
+import {Routes} from "@angular/router";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
-export class RouteService implements OnInit{
-  route :[]
-  constructor() { }
+export class RouteService{
+  private baseUrl = "http://localhost:8080";
+  constructor(private http:HttpClient) { }
 
-  ngOnInit(): void {
+  getAllRoutes():Observable<Routes[]>{
+    return this.http.get<Routes[]>(`${this.baseUrl}/route/get_route`)
   }
 }
