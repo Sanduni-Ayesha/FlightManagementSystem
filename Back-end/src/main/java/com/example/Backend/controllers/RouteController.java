4f4349d01b043adca3e55f11f6c8ab0901761a9d
HarshanaEranda <harshawije1998@gmail.com>
2023-02-01 03:55:48 +0000

    Added deleteRoute method to RouteController class

--- Back-end/src/main/java/com/example/Backend/controllers/RouteController.java
+++ Back-end/src/main/java/com/example/Backend/controllers/RouteController.java
@@ -3,10 +3,7 @@ package com.example.Backend.controllers;
 import com.example.Backend.models.Route;
 import com.example.Backend.services.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +18,10 @@ public class RouteController {
     public List<Route> findAllRoute(){
         System.out.println(routeService.getRoutes());
         return routeService.getRoutes();
+    }
+
+    @DeleteMapping("/delete-route/{id}")
+    public String deleteRoute(@PathVariable("id") int id){
+        return this.routeService.removeRoute(id);
     }
 }
