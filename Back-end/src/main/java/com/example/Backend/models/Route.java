a8c066927f5c843815fc53712579e3c5208a791c
HarshanaEranda <harshawije1998@gmail.com>
2023-02-06 07:27:08 +0000

    Added attributes to  Route class

--- Back-end/src/main/java/com/example/Backend/models/Route.java
+++ Back-end/src/main/java/com/example/Backend/models/Route.java
@@ -1,14 +1,24 @@
 package com.example.Backend.models;
 
+import jakarta.persistence.*;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
+@Entity
+@Table(name = "Routes")
 public class Route {
+
     private int id ;
     private String arrivalAirport;
     private String departureAirport;
     private double mileage;
     private double duration;
-
+    @Version
+    private Long version;
+    private Date createdTime;
+    private Date lastUpdatedTime;
+    private String status;
     public Route(int id, String arrivalAirport, String departureAirport, double mileage, double duration) {
         this.id = id;
         this.arrivalAirport = arrivalAirport;
@@ -17,6 +27,12 @@ public class Route {
         this.duration = duration;
     }
 
+    public Route() {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     public int getId() {
         return id;
     }
@@ -24,7 +40,7 @@ public class Route {
     public void setId(int id) {
         this.id = id;
     }
-
+    @Column(name = "arrivalAirport", nullable = false)
     public String getArrivalAirport() {
         return arrivalAirport;
     }
@@ -32,7 +48,7 @@ public class Route {
     public void setArrivalAirport(String arrivalAirport) {
         this.arrivalAirport = arrivalAirport;
     }
-
+    @Column(name = "departureAirport", nullable = false)
     public String getDepartureAirport() {
         return departureAirport;
     }
@@ -40,7 +56,7 @@ public class Route {
     public void setDepartureAirport(String departureAirport) {
         this.departureAirport = departureAirport;
     }
-
+    @Column(name = "mileage", nullable = false)
     public double getMileage() {
         return mileage;
     }
@@ -48,12 +64,44 @@ public class Route {
     public void setMileage(double mileage) {
         this.mileage = mileage;
     }
-
+    @Column(name = "duration", nullable = false)
     public double getDuration() {
         return duration;
     }
 
     public void setDuration(double duration) {
         this.duration = duration;
+    }
+    @Column(name = "version", nullable = false)
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+    @Column(name = "createdTime", nullable = false)
+    public Date getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(Date createdTime) {
+        this.createdTime = createdTime;
+    }
+    @Column(name = "lastUpdatedTime", nullable = false)
+    public Date getLastUpdatedTime() {
+        return lastUpdatedTime;
+    }
+
+    public void setLastUpdatedTime(Date lastUpdatedTime) {
+        this.lastUpdatedTime = lastUpdatedTime;
+    }
+    @Column(name = "status", nullable = false)
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
