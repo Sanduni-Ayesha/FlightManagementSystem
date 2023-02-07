8f6fe671ab7440b65b05c298a92a7bec65b0b987
HarshanaEranda <harshawije1998@gmail.com>
2023-02-07 11:24:27 +0000

    Changed class name and file name

--- Back-end/src/main/java/com/example/Backend/repositories/RouteRepository.java
+++ Back-end/src/main/java/com/example/Backend/repositories/RouteRepository.java
@@ -1,58 +1,11 @@
 package com.example.Backend.repositories;
 
 import com.example.Backend.models.Route;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Repository
-public class RouteRepository{
-    private List<Route> routes = new ArrayList<Route>();
+public interface RouteRepository extends JpaRepository<Route,Integer> {
 
-    public RouteRepository() {
-        this.routes.add(new Route(1, "Sofia Airport", "Varna Airport", 200, 30));
-        this.routes.add(new Route(2, "Bandaranaike International Colombo Airport", "London Luton Airport", 250, 32));
-        this.routes.add(new Route(3, "Honiara International Airport", "Tokyo Haneda International Airport", 300, 38));
-        this.routes.add(new Route(4, "Indianapolis International Airport", "Turin Airport", 290, 35));
-    }
 
-    public List<Route> getAllRoutes() {
-        return routes;
-    }
-
-    public void deleteRoute(int id) {
-        this.routes.removeIf(route->(route.getId()==id));
-    }
-    public Route findRoute(int id) {
-        for (Route route : this.routes) {
-            if (route.getId() == id) {
-                return route;
-            }
-        }
-        return null;
-    }
-    public int findLastRouteId(){
-        int routeArrayLength = this.routes.size();
-        return this.routes.get(routeArrayLength-1).getId();
-    }
-    public void updateRoute(Route route){
-        Route updatableRoute = findRoute(route.getId());
-        updatableRoute.setArrivalAirport(route.getArrivalAirport());
-        updatableRoute.setDepartureAirport(route.getDepartureAirport());
-        updatableRoute.setDuration(route.getDuration());
-        updatableRoute.setMileage(route.getMileage());
-        this.routes.set(this.routes.indexOf(findRoute(route.getId())),updatableRoute);
-    }
-
-    public void addRoute(Route route){
-        Route newRoute = new Route(
-                this.findLastRouteId()+1,
-                route.getArrivalAirport(),
-                route.getDepartureAirport(),
-                route.getDuration(),
-                route.getMileage());
-        this.routes.add(newRoute);
-
-    }
 }
