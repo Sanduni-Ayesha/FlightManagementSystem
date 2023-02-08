30f6005dbee88f5c6483d4d10e3d6c8fa05a3917
HarshanaEranda <harshawije1998@gmail.com>
2023-02-08 04:10:50 +0000

    Added airport controller class

---
+++ Back-end/src/main/java/com/example/Backend/controllers/AirportController.java
@@ -0,0 +1,26 @@
+package com.example.Backend.controllers;
+
+import com.example.Backend.models.Airport;
+import com.example.Backend.services.AirportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/airport")
+@CrossOrigin("http://localhost:4200")
+public class AirportController {
+    private AirportService airportService;
+    @Autowired
+    public AirportController(AirportService airportService) {
+        this.airportService = airportService;
+    }
+    @GetMapping("/get-airport")
+    public List<Airport> getAllAirport(){
+     return this.airportService.getAllAirports();
+    }
+}
