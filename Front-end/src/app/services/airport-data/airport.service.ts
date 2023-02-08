be46ece86922aa532d656324a219e5011a6091fd
HarshanaEranda <harshawije1998@gmail.com>
2023-02-08 04:24:23 +0000

    Added airport service class

---
+++ Front-end/src/app/services/airport-data/airport.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Airport} from "../../model/Airport";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AirportService {
+  private baseUrl = "http://localhost:8080/airport";
+  constructor(private http:HttpClient) { }
+  public getAllAirports():Observable<Airport[]>{
+    return this.http.get<Airport[]>(`${this.baseUrl}/get-airport`)
+  }
+}
