83a25ec23b6c64553cfd9f877a1b3c91e9b9a334
HarshanaEranda <harshawije1998@gmail.com>
2023-02-08 04:24:10 +0000

    Added airport model class

---
+++ Front-end/src/app/model/Airport.ts
@@ -0,0 +1,9 @@
+export class Airport{
+    airport_code:String;
+    airport_name:String;
+
+    constructor(airport_code: String, airport_name: String) {
+        this.airport_code = airport_code;
+        this.airport_name = airport_name;
+    }
+}
