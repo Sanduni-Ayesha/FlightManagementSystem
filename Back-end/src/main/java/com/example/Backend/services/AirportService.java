a2c4984788839c6eeff30994f66d848235825f73
HarshanaEranda <harshawije1998@gmail.com>
2023-02-08 04:01:55 +0000

    Added airport service class

---
+++ Back-end/src/main/java/com/example/Backend/services/AirportService.java
@@ -0,0 +1,22 @@
+package com.example.Backend.services;
+
+import com.example.Backend.models.Airport;
+import com.example.Backend.repositories.AirportRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AirportService {
+    private AirportRepository airportRepository;
+    @Autowired
+    public AirportService(AirportRepository airportRepository) {
+        this.airportRepository = airportRepository;
+    }
+
+    public List<Airport> getAllAirports(){
+        List<Airport> airports = airportRepository.findAll();
+        return airports;
+    }
+}
