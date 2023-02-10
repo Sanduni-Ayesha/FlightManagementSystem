9736a5ac8fab7c05632f66b392e4da065c76e0b0
HarshanaEranda <harshawije1998@gmail.com>
2023-02-10 07:49:25 +0000

    Implemented the method

--- Back-end/src/main/java/com/example/Backend/daoImpl/RouteDaoImplementation.java
+++ Back-end/src/main/java/com/example/Backend/daoImpl/RouteDaoImplementation.java
@@ -2,12 +2,33 @@ package com.example.Backend.daoImpl;
 
 import com.example.Backend.dao.RouteDao;
 import com.example.Backend.models.Route;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import java.util.List;
 
 public class RouteDaoImplementation implements RouteDao<Route> {
+    private JdbcTemplate jdbcTemplate;
     @Override
-    public List<Route> searchRoute() {
+    public List<Route> searchRoute(String departureAirport, String arrivalAirport) {
+        String table = "route";
+        String query = "SELECT * FROM "+table;
+        if(departureAirport.isEmpty() && arrivalAirport.isEmpty() ){
+           List<Route> allRoutesDetails= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
+            return allRoutesDetails;
+        }
+        else if (departureAirport.isEmpty() && !arrivalAirport.isEmpty() ) {
+            List<Route> searchedRoutesDetailsByDeparture= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
+            return searchedRoutesDetailsByDeparture;
+        }
+        else if (!departureAirport.isEmpty() && arrivalAirport.isEmpty() ) {
+            List<Route> searchedRoutesDetailsByArrival= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
+            return searchedRoutesDetailsByArrival;
+        }
+        else if (!departureAirport.isEmpty() && arrivalAirport.isEmpty() ) {
+            List<Route> searchedRoutesDetailsByArrival= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
+            return searchedRoutesDetailsByArrival;
+        }
         return null;
     }
 }
