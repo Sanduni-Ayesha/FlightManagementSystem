be03adf304981d2a464de042021406e0e92d6472
HarshanaEranda <harshawije1998@gmail.com>
2023-02-10 10:03:21 +0000

    Added Repository annotation

--- Back-end/src/main/java/com/example/Backend/dao/RouteDao.java
+++ Back-end/src/main/java/com/example/Backend/dao/RouteDao.java
@@ -2,6 +2,9 @@ package com.example.Backend.dao;
 
 import java.util.List;
 import com.example.Backend.models.Route;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface RouteDao<T> {
     List<Route> searchRoute(String departureAirport,String arrivalAirport);
 }
