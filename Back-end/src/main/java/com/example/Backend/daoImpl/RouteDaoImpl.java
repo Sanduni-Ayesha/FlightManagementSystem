c8a68398a53b7d9c70109697ddd91e106bef7cd5
HarshanaEranda <harshawije1998@gmail.com>
2023-02-10 09:59:31 +0000

    Changed file name

---
+++ Back-end/src/main/java/com/example/Backend/daoImpl/RouteDaoImpl.java
@@ -0,0 +1,35 @@
+package com.example.Backend.daoImpl;
+
+import com.example.Backend.dao.RouteDao;
+import com.example.Backend.models.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class RouteDaoImpl implements RouteDao<Route> {
+
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    public RouteDaoImpl(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public List<Route> searchRoute(String departureAirport, String arrivalAirport) {
+        String table = "route";
+        String query = "SELECT * FROM route WHERE status='active'";
+        if(!(departureAirport.equals(""))){
+            query+=" and departure_airport = " + departureAirport;
+        }
+        if (!(arrivalAirport.equals(""))) {
+            query+=" and arrival_airport = " + arrivalAirport;
+        }
+        List<Route> searchedRoutesDetails= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
+        return searchedRoutesDetails;
+
+    }
+}
