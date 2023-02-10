82eccd409ef01a6ca06b745dfb887e195463bf41
HarshanaEranda <harshawije1998@gmail.com>
2023-02-10 07:48:59 +0000

    Added method parameters

--- Back-end/src/main/java/com/example/Backend/dao/RouteDao.java
+++ Back-end/src/main/java/com/example/Backend/dao/RouteDao.java
@@ -3,5 +3,5 @@ package com.example.Backend.dao;
 import java.util.List;
 import com.example.Backend.models.Route;
 public interface RouteDao<T> {
-    List<Route> searchRoute();
+    List<Route> searchRoute(String departureAirport,String arrivalAirport);
 }
