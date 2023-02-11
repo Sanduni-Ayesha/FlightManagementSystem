6c5b34d650d67cce8820a86ad6cba5271380d8f3
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-02-11 10:49:22 +0000

    create flightDTO

--- Back-end/src/main/java/com/example/Backend/dto/FlightDto.java
+++ Back-end/src/main/java/com/example/Backend/dto/FlightDto.java
@@ -1,4 +1,26 @@
 package com.example.Backend.dto;
 
+import com.example.Backend.models.Flight;
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Version;
+
+import java.time.LocalDateTime;
+
 public class FlightDto {
+    private String departureAirport;
+    private String arrivalAirport;
+    private String flightNo;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private LocalDateTime createdTime;
+    private LocalDateTime lastUpdatedTime;
+    private Status status;
+    private int version;
+
+    public enum Status{
+        active,
+        inactive
+    }
 }
