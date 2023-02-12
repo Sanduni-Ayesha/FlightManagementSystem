4cfd56e60a734650aed379508e77c75b469e0623
HarshanaEranda <harshawije1998@gmail.com>
2023-02-12 13:54:00 +0000

    Added new abstract methods

--- Back-end/src/main/java/com/example/Backend/repositories/RouteRepository.java
+++ Back-end/src/main/java/com/example/Backend/repositories/RouteRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RouteRepository extends JpaRepository<Route,Integer> {
-
-
+    Route findRouteById(int id);
+    Boolean existsRouteByArrivalAirportAndDepartureAirportAndStatus(String arrivalAirport,String departureAirport,Route.Status status);
+    Boolean existsRouteByIdAndStatusEquals(int id,Route.Status status);
 }
