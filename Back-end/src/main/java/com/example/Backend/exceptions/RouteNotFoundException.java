0999fdcb80c506f0a1a7ec4a3df58ace13a52b6f
HarshanaEranda <harshawije1998@gmail.com>
2023-02-13 04:05:50 +0000

    Deleted file

--- Back-end/src/main/java/com/example/Backend/exceptions/RouteNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.Backend.exceptions;
-
-public class RouteNotFoundException extends RuntimeException{
-    public RouteNotFoundException(String errorMessage){
-        super(errorMessage);
-    }
-}
