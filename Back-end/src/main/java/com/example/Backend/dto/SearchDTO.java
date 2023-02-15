1dbb5287c93d179a3e621eae49c28f51fec451ef
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-02-15 12:04:50 +0000

    search DTO added

---
+++ Back-end/src/main/java/com/example/Backend/dto/SearchDTO.java
@@ -0,0 +1,22 @@
+package com.example.Backend.dto;
+
+public class SearchDTO {
+    private String arrivalAirport;
+    private String departureAirport;
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+}
