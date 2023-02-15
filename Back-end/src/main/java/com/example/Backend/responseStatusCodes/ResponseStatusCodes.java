4489297a1ca4a7c8f416d66d6e059814e1956200
HarshanaEranda <harshawije1998@gmail.com>
2023-02-15 07:21:01 +0000

    Resolved conflict

---
+++ Back-end/src/main/java/com/example/Backend/responseStatusCodes/ResponseStatusCodes.java
@@ -0,0 +1,31 @@
+package com.example.Backend.responseStatusCodes;
+
+public enum ResponseStatusCodes {
+    FLIGHT_ADD_SUCCESSFUL(230),
+    FLIGHT_DELETE_SUCCESSFUL(231),
+    FLIGHT_UPDATE_SUCCESSFUL(232),
+    ROUTE_NOT_EXISTS_EXCEPTION(233),
+    INVALID_ROUTE_EXCEPTION(234),
+    ROUTE_EXISTS_EXCEPTION(235),
+    ROUTE_ALREADY_UPDATED_EXCEPTION(236),
+    FLIGHT_NOT_FOUND_EXCEPTION(237),
+    FLIGHT_ALREADY_DELETED_EXCEPTION(238),
+    ROUTE_ALREADY_DELETED_EXCEPTION(242),
+    FLIGHT_EXISTS_EXCEPTION(239),
+    INVALID_FLIGHT_EXCEPTION(240),
+    FLIGHT_ALREADY_UPDATED_EXCEPTION(241),
+    DEPARTURE_AIRPORT_AND_ARRIVAL_AIRPORT_SAME_EXCEPTION(243)
+    ;
+
+    private int responseCode;
+    ResponseStatusCodes(int responseCode) {
+        this.responseCode = responseCode;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+    public static int valueOf(ResponseStatusCodes responseStatusCodes){
+        return responseStatusCodes.responseCode;
+    }
+}
