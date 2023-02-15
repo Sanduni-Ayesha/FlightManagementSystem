d8d2722e074b59bf5a274174dfea6cba7d86785e
HarshanaEranda <harshawije1998@gmail.com>
2023-02-15 09:52:28 +0000

    Added new method and aligned

--- Back-end/src/main/java/com/example/Backend/dtoMapper/RouteMapper.java
+++ Back-end/src/main/java/com/example/Backend/dtoMapper/RouteMapper.java
@@ -4,7 +4,7 @@ import com.example.Backend.dto.RouteDto;
 import com.example.Backend.models.Route;
 
 public class RouteMapper {
-    public static Route routeDtoToRouteMapper(RouteDto routeDto){
+    public static Route routeDtoToRouteMapper(RouteDto routeDto) {
         Route route = new Route();
         route.setId(routeDto.getId());
         route.setArrivalAirport(routeDto.getArrivalAirport());
@@ -15,5 +15,18 @@ public class RouteMapper {
         route.setCreatedTime(routeDto.getCreatedTime());
         route.setLastUpdatedTime(routeDto.getLastUpdatedTime());
         return route;
+    }
+
+    public static RouteDto routeToRouteDtoMapper(Route route) {
+        RouteDto routeDto = new RouteDto();
+        routeDto.setId(route.getId());
+        routeDto.setArrivalAirport(route.getArrivalAirport());
+        routeDto.setDepartureAirport(route.getDepartureAirport());
+        routeDto.setMileage(route.getMileage());
+        routeDto.setDuration(route.getDuration());
+        routeDto.setVersion(route.getVersion());
+        routeDto.setCreatedTime(route.getCreatedTime());
+        routeDto.setLastUpdatedTime(route.getLastUpdatedTime());
+        return routeDto;
     }
 }
