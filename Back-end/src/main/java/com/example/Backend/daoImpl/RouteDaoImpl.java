ae76613a065c1c6c2e40ff40d3d3145f6daea871
HarshanaEranda <harshawije1998@gmail.com>
2023-02-16 12:50:17 +0000

    Changed method implementation

--- Back-end/src/main/java/com/example/Backend/daoImpl/RouteDaoImpl.java
+++ Back-end/src/main/java/com/example/Backend/daoImpl/RouteDaoImpl.java
@@ -3,6 +3,7 @@ package com.example.Backend.daoImpl;
 import com.example.Backend.dao.RouteDao;
 import com.example.Backend.dto.SearchDTO;
 import com.example.Backend.models.Route;
+import com.example.Backend.queryBuilder.RouteQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -21,14 +22,7 @@ public class RouteDaoImpl implements RouteDao<Route> {
 
     @Override
     public List<Route> searchRoute(SearchDTO searchDTO) {
-        String table = "route";
-        String query = "SELECT * FROM route WHERE status='active'";
-        if(searchDTO.getDepartureAirport()!=null && !searchDTO.getDepartureAirport().equals("")){
-            query+="and departure_airport ='" + searchDTO.getDepartureAirport()+"'";
-        }
-        if (searchDTO.getArrivalAirport()!=null && !searchDTO.getArrivalAirport().equals("")) {
-            query+=" and arrival_airport ='" + searchDTO.getArrivalAirport()+"'";
-        }
+        String query = RouteQueryBuilder.searchQuery(searchDTO);
         List<Route> searchedRoutesDetails= this.jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Route.class));
         return searchedRoutesDetails;
 
