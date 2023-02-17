49a33ebcb11054b05d008f4af83884e40cc51f61
HarshanaEranda <harshawije1998@gmail.com>
2023-02-17 08:56:56 +0000

    Added new dto class for scheduling flight

---
+++ Back-end/src/main/java/com/example/Backend/dto/ScheduleFlightDto.java
@@ -0,0 +1,105 @@
+package com.example.Backend.dto;
+
+import java.time.LocalDateTime;
+
+public class ScheduleFlightDto {
+    private int id;
+    private String departureAirport;
+    private String arrivalAirport;
+    private String flightNo;
+    private LocalDateTime departureTime;
+    private LocalDateTime arrivalTime;
+    private LocalDateTime createdTime;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+    private String[] weekdays;
+    private int version = 1;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDepartureAirport() {
+        return departureAirport;
+    }
+
+    public void setDepartureAirport(String departureAirport) {
+        this.departureAirport = departureAirport;
+    }
+
+    public String getArrivalAirport() {
+        return arrivalAirport;
+    }
+
+    public void setArrivalAirport(String arrivalAirport) {
+        this.arrivalAirport = arrivalAirport;
+    }
+
+    public String getFlightNo() {
+        return flightNo;
+    }
+
+    public void setFlightNo(String flightNo) {
+        this.flightNo = flightNo;
+    }
+
+    public LocalDateTime getDepartureTime() {
+        return departureTime;
+    }
+
+    public void setDepartureTime(LocalDateTime departureTime) {
+        this.departureTime = departureTime;
+    }
+
+    public LocalDateTime getArrivalTime() {
+        return arrivalTime;
+    }
+
+    public void setArrivalTime(LocalDateTime arrivalTime) {
+        this.arrivalTime = arrivalTime;
+    }
+
+    public LocalDateTime getCreatedTime() {
+        return createdTime;
+    }
+
+    public void setCreatedTime(LocalDateTime createdTime) {
+        this.createdTime = createdTime;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
+
+    public String[] getWeekdays() {
+        return weekdays;
+    }
+
+    public void setWeekdays(String[] weekdays) {
+        this.weekdays = weekdays;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+}
