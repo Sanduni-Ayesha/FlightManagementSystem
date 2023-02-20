56fda70cca8ebdf5825b0c2b9aef0f05b708c393
HarshanaEranda <harshawije1998@gmail.com>
2023-02-20 08:47:56 +0000

    Added constructor

--- Back-end/src/main/java/com/example/Backend/services/ScheduleFlightService.java
+++ Back-end/src/main/java/com/example/Backend/services/ScheduleFlightService.java
@@ -1,21 +1,38 @@
 package com.example.Backend.services;
 
+import com.example.Backend.daoImpl.FlightDaoImpl;
+import com.example.Backend.dto.FlightDto;
 import com.example.Backend.dto.ScheduleFlightDto;
+import com.example.Backend.dtoMapper.FlightMapper;
 import com.example.Backend.dtoMapper.FlightSchedulerMapper;
 import com.example.Backend.models.Flight;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 @Service
 public class ScheduleFlightService {
+    private FlightDaoImpl flightDaoImpl;
+    @Autowired
+    public ScheduleFlightService(FlightDaoImpl flightDaoImpl) {
+        this.flightDaoImpl = flightDaoImpl;
+    }
+
+    @Transactional
     public ScheduleFlightDto createSchedule(ScheduleFlightDto scheduleFlightDto){
         FlightSchedulerMapper flightSchedulerMapper = new FlightSchedulerMapper();
         List<Flight> flights = flightSchedulerMapper.convertScheduleFlightDtoToFlight(scheduleFlightDto);
-        for(Flight f:flights){
-            System.out.println(f.getDepartureTime());
-            System.out.println(f.getArrivalTime());
-            System.out.println("==================");
+        for(Flight flight:flights){
+            if(checkFlightDuplicates(FlightMapper.flightToFlightDtoMapper(flight))){
+
+            }
         }
         return  scheduleFlightDto;
+    }
+    @Transactional(propagation = Propagation.MANDATORY)
+    public boolean checkFlightDuplicates(FlightDto flightDto) {
+        return flightDaoImpl.checkDuplicate(flightDto);
     }
 }
