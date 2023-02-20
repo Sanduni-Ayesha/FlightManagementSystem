bd69346e6ffc94e1702a62452e4b980808cdb6b6
HarshanaEranda <harshawije1998@gmail.com>
2023-02-20 08:30:47 +0000

    Added new controller class

---
+++ Back-end/src/main/java/com/example/Backend/controllers/ScheduleFlightController.java
@@ -0,0 +1,27 @@
+package com.example.Backend.controllers;
+
+import com.example.Backend.dto.ScheduleFlightDto;
+import com.example.Backend.services.FlightService;
+import com.example.Backend.services.ScheduleFlightService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/flight")
+public class ScheduleFlightController {
+    private ScheduleFlightService scheduleFlightService;
+    @Autowired
+    public ScheduleFlightController(ScheduleFlightService scheduleFlightService) {
+        this.scheduleFlightService = scheduleFlightService;
+    }
+
+    @PostMapping("/schedule-flight-service")
+    public ResponseEntity<ScheduleFlightDto> createScheduleFlight(@RequestBody ScheduleFlightDto scheduleFlightDto){
+        return new ResponseEntity<>(scheduleFlightService.createSchedule(scheduleFlightDto), HttpStatus.OK);
+    }
+}
