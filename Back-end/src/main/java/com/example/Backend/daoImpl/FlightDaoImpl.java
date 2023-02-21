24ed8d01448e0f6f6d8e90617f6ee9af6617f259
HarshanaEranda <harshawije1998@gmail.com>
2023-02-21 11:57:18 +0000

    Override method

--- Back-end/src/main/java/com/example/Backend/daoImpl/FlightDaoImpl.java
+++ Back-end/src/main/java/com/example/Backend/daoImpl/FlightDaoImpl.java
@@ -1,6 +1,7 @@
 package com.example.Backend.daoImpl;
 
 import com.example.Backend.dto.FlightDto;
+import com.example.Backend.dto.ScheduleFlightDto;
 import com.example.Backend.dto.SearchDTO;
 import com.example.Backend.models.Flight;
 import com.example.Backend.dao.FlightDao;
@@ -33,13 +34,13 @@ public class FlightDaoImpl implements FlightDao {
     private String createQueryForSearch(SearchDTO searchDTO) {
         List<String> queryList = new ArrayList<>();
         if (!searchDTO.getDepartureAirport().equals("")) {
-            queryList.add("departure_airport='"+searchDTO.getDepartureAirport() + "'");
+            queryList.add("departure_airport='" + searchDTO.getDepartureAirport() + "'");
         }
         if (!searchDTO.getArrivalAirport().equals("")) {
-            queryList.add("arrival_airport='"+searchDTO.getArrivalAirport() + "'");
+            queryList.add("arrival_airport='" + searchDTO.getArrivalAirport() + "'");
         }
         String finalString = String.join(" AND ", queryList);
-        if(!queryList.isEmpty()) {
+        if (!queryList.isEmpty()) {
             return (" AND " + finalString);
         }
         return "";
@@ -48,9 +49,9 @@ public class FlightDaoImpl implements FlightDao {
     @Override
     public boolean checkDuplicate(FlightDto flightDto) {
         String sqlCheckDuplicates = "Select * from flight where " +
-                "(departure_time<'" + flightDto.getDepartureTime() + "'and arrival_time>'" + flightDto.getDepartureTime() + "') or" +
-                "(departure_time<'" + flightDto.getArrivalTime() + "'and arrival_time>'" + flightDto.getArrivalTime() + "') or" +
-                "(departure_time<'" + flightDto.getDepartureTime() + "'and arrival_time>'" + flightDto.getArrivalTime() + "')" +
+                "(departure_time<='" + flightDto.getDepartureTime() + "'and arrival_time>='" + flightDto.getDepartureTime() + "') or" +
+                "(departure_time<='" + flightDto.getArrivalTime() + "'and arrival_time>='" + flightDto.getArrivalTime() + "') or" +
+                "(departure_time>='" + flightDto.getDepartureTime() + "'and arrival_time<='" + flightDto.getArrivalTime() + "')" +
                 "and flight_no='" + flightDto.getFlightNo() + "'";
         List<Flight> flights = jdbcTemplate.query(sqlCheckDuplicates, BeanPropertyRowMapper.newInstance(Flight.class));
         if (flights.size() > 1) {
@@ -58,4 +59,20 @@ public class FlightDaoImpl implements FlightDao {
         }
         return false;
     }
+    @Override
+    public Flight checkDuplicateByAllScheduleFlights(ScheduleFlightDto scheduleFlightDto) {
+        String query = " SELECT * FROM flight WHERE flight_no='" + scheduleFlightDto.getFlightNo() + "' AND" + " departure_time between '" +
+                scheduleFlightDto.getStartDate() + "' AND '" + scheduleFlightDto.getEndDate() + "' AND (";
+        for (int index = 0; index < scheduleFlightDto.getWeekdays().size() - 1; index++) {
+            query += "UPPER(dayname(departure_time)) = '" + scheduleFlightDto.getWeekdays().get(index) + "' OR ";
+        }
+        query += "UPPER(dayname(departure_time)) = '" + scheduleFlightDto.getWeekdays().get(scheduleFlightDto.getWeekdays().size() - 1) + "')"
+                + "ORDER BY departure_time limit 1";
+        List<Flight> flights = jdbcTemplate.query(query, BeanPropertyRowMapper.newInstance(Flight.class));
+        if (flights.size() == 0) {
+            return null;
+        }
+        return flights.get(0);
+    }
+
 }
