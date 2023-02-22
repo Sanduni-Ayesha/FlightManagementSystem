8bc0a0c1e3e9051461a9bda757368e2bd3a4db9b
Sanduni Ayesha <sanduniayesha.19@cse.mrt.ac.lk>
2023-02-22 03:53:12 +0000

    alert-modal code formatted

--- Front-end/src/app/component/Alert/alert-modal/alert-modal.component.ts
+++ Front-end/src/app/component/Alert/alert-modal/alert-modal.component.ts
@@ -3,21 +3,22 @@ import {MatDialog} from "@angular/material/dialog";
 import {DIALOG_DATA} from "@angular/cdk/dialog";
 
 @Component({
-  selector: 'app-alert-modal',
-  templateUrl: './alert-modal.component.html',
-  styleUrls: ['./alert-modal.component.scss']
+    selector: 'app-alert-modal',
+    templateUrl: './alert-modal.component.html',
+    styleUrls: ['./alert-modal.component.scss']
 })
-export class AlertModalComponent implements OnInit{
-  private title: string = "";
-  private message: string = "";
+export class AlertModalComponent implements OnInit {
+    private title: string = "";
+    private message: string = "";
 
-  constructor(
-      public dialog: MatDialog,
-      @Inject(DIALOG_DATA) public data: any,
-  ) {
-  }
-  ngOnInit() {
-    this.title = this.data.title;
-    this.message = this.data.message;
-  }
+    constructor(
+        public dialog: MatDialog,
+        @Inject(DIALOG_DATA) public data: any,
+    ) {
+    }
+
+    ngOnInit() {
+        this.title = this.data.title;
+        this.message = this.data.message;
+    }
 }
